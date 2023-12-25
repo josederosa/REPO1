d40520e7d3dcb838b020476c74f8960f4558a95f
josederosa <josedaniel.derosa@outlook.com>
2023-12-25 14:08:19 -0300

    primer commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+
+
+if (func1() && func2())
+    Console.WriteLine("Pasó");
+else
+    Console.WriteLine("NO Pasó");
+
+
+Console.ReadLine();
+
+
+bool func1()
+{
+    Console.WriteLine("func1");
+return false;
+}
+bool func2()
+{
+    Console.WriteLine("func2");
+return true;
+}
