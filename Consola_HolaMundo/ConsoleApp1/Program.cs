8443285488c5fa8ca80a23f3f31516d71770fe9a
josederosa <josedaniel.derosa@outlook.com>
2023-12-25 14:14:24 -0300

    primer commit

---
+++ Consola_HolaMundo/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Hola();
+Mundo();
+Bucle();
+
+Console.ReadLine();
+
+void Hola()
+{
+    Console.WriteLine("Hola");
+}
+void Mundo()
+{
+    Console.WriteLine("Mundo");
+}
+void Bucle()
+{ 
+    for (int i = 0; i < 20; i++)
+    {
+        Console.WriteLine(i+1);
+    }
+}
